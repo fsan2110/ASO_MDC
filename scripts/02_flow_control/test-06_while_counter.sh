fd71b0fce58cd04d64eac387a25819c0001a3c90
fsan2110 <fsanrui2110b@g.educaand.es>
2025-11-11 20:14:14 +0100

    Comentarios añadidos

--- scripts/02_flow_control/test-06_while_counter.sh
+++ scripts/02_flow_control/test-06_while_counter.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 test-counter() {
+# almacena la salida del script
 output=$(echo "$2" | scripts/02_flow_control/06_while_counter.sh)
+# si el código y la salida son correctos
 if [[ $? -eq $1 && $output == *"$3"* ]]; then
 	echo "Prueba superada"
 else
@@ -10,6 +12,8 @@ else
 fi
 }
 
+# Prueba de argumento vacío
 test-counter 1 " " "Error: escriba un número natural (1,2,3...)"
 
+# Prueba normal
 test-counter 0 "4" "Contador finalizado. Se han mostrado 4 números."
